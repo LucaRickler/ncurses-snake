02f5440f9e79a98c5db28dd65a28838022ece10c
Luca Rickler <luca.rickler@gmail.com>
2018-09-13 10:48:06 +0200

    SIGINT

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <signal.h>
 
 using std::cout;
 using std::endl;
 
 void ClearTerm() {
-    std::cout << "\033[2J\033[1;1H"; 
+    std::cout << "\033[2J\033[1;1H";
+}
+
+void SigIntHandler(int param) {
+    ClearTerm();
+    exit(1);
 }
 
 int main() {
-    // ClearTerm();
-    // for (int i=10; i>0; --i) {
-    //     std::cout << "countdown: " << i << std::endl;
-    //     std::this_thread::sleep_for (std::chrono::seconds(1));
-    //     ClearTerm();
-    // }
-    // std::cout << "Lift off!\n";
+    signal(SIGINT, SigIntHandler);
 
-    // return 0;
     ClearTerm();
     for (int i = 0;; i++) {    
         cout << "Cicle N " << i << endl;
