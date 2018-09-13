e5eaa5dfd8add1ec1948c94c7d5619d4276b3ace
Luca Rickler <luca.rickler@gmail.com>
2018-09-13 10:31:51 +0200

    ClearTerm & Timing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+using std::cout;
+using std::endl;
+
+void ClearTerm() {
+    std::cout << "\033[2J\033[1;1H"; 
+}
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    // ClearTerm();
+    // for (int i=10; i>0; --i) {
+    //     std::cout << "countdown: " << i << std::endl;
+    //     std::this_thread::sleep_for (std::chrono::seconds(1));
+    //     ClearTerm();
+    // }
+    // std::cout << "Lift off!\n";
+
+    // return 0;
+    ClearTerm();
+    for (int i = 0;; i++) {    
+        cout << "Cicle N " << i << endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(20)); 
+        ClearTerm();
+    }
+    return 0;
 }
